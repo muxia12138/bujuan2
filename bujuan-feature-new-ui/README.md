17d6ac0b429b85cc792df1b3303b4ee4fc1fcc40
muxia12138 <113419200+muxia12138@users.noreply.github.com>
2025-09-01 23:04:54 +0800

    Add files via upload

---
+++ bujuan-feature-new-ui/README.md
@@ -0,0 +1,46 @@
+<p align="center">
+<img src="macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_256.png" alt="app_icon_256" style="zoom: 50%;"/>
+</p>
+<h1 align="center">bujuan</h1>
+<p align="center">一个支持Android、iOS、MacOS、Windows、Linux的三方网易云播放器。</p>
+
+#### MacOS 
+
+![](gif/desktop1.png)
+![](gif/desktop2.png)
+![](gif/desktop3.png)
+![](gif/home.gif)
+
+
+
+#### Android & IOS
+
+| <img src="gif/phone1.jpg" alt="956" style="zoom:20%;" /> | <img src="gif/phone2.jpg" alt="954" style="zoom:20%;" /> | <img src="gif/phone3.jpg" alt="955" style="zoom:20%;" /> |
+|----------------------------------------------------------|----------------------------------------------------------|----------------------------------------------------------|
+
+
+
+
+
+
+
+## 功能介绍
+- 采用flutter开发，可打包编译为所有平台。
+
+- 网易云账号登录（手机号、二维码）。
+
+- 每日推荐、私人Fm、电台、云盘。
+
+- 歌手、专辑、搜索
+
+- 去除无用功能，专注于用户音乐。
+
+
+
+
+
+### 代码部署编译
+- `flutter pub get`
+- `flutter build apk --release`
+- `flutter build ios`
+- `flutter build macos`
---
+++ bujuan-feature-new-ui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audio_service
+import audio_session
+import audioplayers_darwin
+import path_provider_foundation
+import rive_native
+import screen_retriever_macos
+import sqflite_darwin
+import window_manager
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RiveNativePlugin.register(with: registry.registrar(forPlugin: "RiveNativePlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
+}
